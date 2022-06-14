d129d35ca8a6571e7219e94fe1a49e7094ea3154
Lovro Markovic <lovro.markovic@fer.hr>
2022-06-14 16:16:46 +0200

    Check controller and tracker inputs

--- nodes/uav_safety_node.cpp
+++ nodes/uav_safety_node.cpp
@@ -4,6 +4,9 @@
 #include <geometry_msgs/PoseArray.h>
 #include <std_srvs/SetBool.h>
 #include <std_srvs/Empty.h>
+#include <larics_motion_planning/CheckStateValidity.h>
+#include <uav_ros_lib/ros_convert.hpp>
+#include <uav_ros_lib/param_util.hpp>
 
 enum class SafetyStates { IDLE, OVERRIDE };
 
@@ -49,7 +52,7 @@ public:
       return "OVERRIDE";
 
     default:
-      return "NONE";
+      return "ERROR";
     }
   }
 
@@ -58,13 +61,22 @@ private:
   SafetyStates m_current_state = SafetyStates::IDLE;
 };
 
+struct SafetyNodeParams
+{
+  bool check_position_hold        = true;
+  bool check_tracker_pose         = true;
+  bool check_tracker_trajectory   = true;
+  bool check_remaining_trajectory = true;
+};
+
 class UavSafetyNode
 {
 public:
   UavSafetyNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
 
 private:
-  SafetyMachine m_safety_sm;
+  SafetyMachine    m_safety_sm;
+  SafetyNodeParams m_safety_params;
 
   ros::Subscriber m_position_hold_sub;
   ros::Publisher  m_position_hold_final_pub;
@@ -85,10 +97,29 @@ private:
   ros::ServiceServer m_safety_override_srv;
   bool               safety_override_cb(std_srvs::SetBool::Request&  req,
                                         std_srvs::SetBool::Response& resp);
+
+  static constexpr auto WATCHDOG_RATE = 50.0;
+  ros::Timer            m_traj_watchdog_timer;
+  void                  trajectory_watchdog_cb(const ros::TimerEvent& /*unused*/);
+
+  std::mutex         m_trajectory_checker_mutex;
+  ros::ServiceClient m_trajectory_checker_client;
+  void               check_trajectory(const trajectory_msgs::JointTrajectory& traj);
 };
 
 UavSafetyNode::UavSafetyNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private)
 {
+  param_util::getParamOrThrow(
+    nh_private, "safety/check_position_hold", m_safety_params.check_position_hold);
+  param_util::getParamOrThrow(
+    nh_private, "safety/check_tracker_pose", m_safety_params.check_tracker_pose);
+  param_util::getParamOrThrow(nh_private,
+                              "safety/check_tracker_trajectory",
+                              m_safety_params.check_tracker_trajectory);
+  param_util::getParamOrThrow(nh_private,
+                              "safety/check_remaining_trajectory",
+                              m_safety_params.check_remaining_trajectory);
+
   m_carrot_pose_array_sub = nh.subscribe(
     "tracker/remaining_trajectory", 1, &UavSafetyNode::carrot_pose_array_cb, this);
 
@@ -110,6 +141,57 @@ UavSafetyNode::UavSafetyNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private)
   m_safety_override_srv =
     nh.advertiseService("safety/override", &UavSafetyNode::safety_override_cb, this);
   m_tracker_reset_client = nh.serviceClient<std_srvs::Empty>("tracker/reset");
+
+  m_traj_watchdog_timer = nh.createTimer(
+    ros::Duration(1 / WATCHDOG_RATE), &UavSafetyNode::trajectory_watchdog_cb, this);
+
+  m_trajectory_checker_client =
+    nh.serviceClient<larics_motion_planning::CheckStateValidity>("validity_checker");
+}
+
+void UavSafetyNode::check_trajectory(const trajectory_msgs::JointTrajectory& traj)
+{
+  larics_motion_planning::CheckStateValidity state_validity;
+
+  {
+    std::lock_guard<std::mutex> lock(m_trajectory_checker_mutex);
+    state_validity.request.points = traj;
+    auto success                  = m_trajectory_checker_client.call(state_validity);
+    if (!success) {
+      ROS_FATAL(
+        "[UavSafetyNode::check_trajectory] - validity_checker server unavailable.");
+      return;
+    }
+  }
+
+  if (!state_validity.response.valid) {
+    ROS_WARN("COLLISION DETECTED, AUTOMATIC OVERRIDE!");
+    std_srvs::SetBool::Request      req;
+    std_srvs::SetBool::ResponseType resp;
+    req.data     = true;
+    auto success = safety_override_cb(req, resp);
+    if (!success || !resp.success) {
+      ROS_ERROR(
+        "[UavSafetyNode::check_trajectory] - unable to trigger override, this shouldn't "
+        "happen.");
+      return;
+    }
+  }
+}
+
+void UavSafetyNode::trajectory_watchdog_cb(const ros::TimerEvent& /*unused*/)
+{
+  if (m_safety_sm.isOverride()) {
+    ROS_INFO_THROTTLE(
+      2.0, "[UavSafetyNode::trajectory_watchdog] Current state is OVERRIDE, no danger.");
+    return;
+  }
+
+  if (!m_trajectory_checker_client.exists()) {
+    ROS_WARN_THROTTLE(2.0,
+                      "[UavSafetyNode::trajectory_watchdog] Checker service is missing!");
+    return;
+  }
 }
 
 bool UavSafetyNode::safety_override_cb(std_srvs::SetBool::Request&  req,
@@ -137,6 +219,14 @@ bool UavSafetyNode::safety_override_cb(std_srvs::SetBool::Request&  req,
 void UavSafetyNode::position_hold_cb(
   const trajectory_msgs::MultiDOFJointTrajectoryPoint& msg)
 {
+  if (m_safety_params.check_position_hold) {
+    // First check the trajectory
+    trajectory_msgs::JointTrajectory joint_trajectory;
+    joint_trajectory.points.emplace_back(ros_convert::pose_to_joint_trajectory_point(
+      ros_convert::trajectory_point_to_pose(msg)));
+    check_trajectory(joint_trajectory);
+  }
+
   if (m_safety_sm.isOverride()) {
     ROS_WARN_THROTTLE(
       2.0, "[UavSafetyNode] Override enabled. position_hold/trajectory rejected");
@@ -150,10 +240,19 @@ void UavSafetyNode::position_hold_cb(
 void UavSafetyNode::carrot_pose_array_cb(const geometry_msgs::PoseArray& msg)
 {
   ROS_INFO_THROTTLE(3.0, "[UavSafetyNode] Got remaining trajectory.");
+  if (!m_safety_params.check_remaining_trajectory) { return; }
 }
 
 void UavSafetyNode::tracker_pose_cb(const geometry_msgs::PoseStamped& msg)
 {
+  if (m_safety_params.check_tracker_pose) {
+    // Check the tracker pose
+    trajectory_msgs::JointTrajectory joint_trajectory;
+    joint_trajectory.points.push_back(
+      ros_convert::pose_to_joint_trajectory_point(msg.pose));
+    check_trajectory(joint_trajectory);
+  }
+
   if (m_safety_sm.isOverride()) {
     ROS_WARN_THROTTLE(2.0,
                       "[UavSafetyNode] Override enabled. tracker/input_pose rejected");
@@ -167,6 +266,18 @@ void UavSafetyNode::tracker_pose_cb(const geometry_msgs::PoseStamped& msg)
 void UavSafetyNode::tracker_trajectory_cb(
   const trajectory_msgs::MultiDOFJointTrajectory& msg)
 {
+  if (m_safety_params.check_tracker_trajectory) {
+    // Check tracker trajectory
+    trajectory_msgs::JointTrajectory joint_trajectory;
+    std::for_each(msg.points.begin(),
+                  msg.points.end(),
+                  [&](const trajectory_msgs::MultiDOFJointTrajectoryPoint& point) {
+                    joint_trajectory.points.emplace_back(
+                      ros_convert::trajectory_point_to_joint_trajectory_point(point));
+                  });
+    check_trajectory(joint_trajectory);
+  }
+
   if (m_safety_sm.isOverride()) {
     ROS_WARN_THROTTLE(
       2.0, "[UavSafetyNode] Override enabled. tracker/input_trajectory rejected");
